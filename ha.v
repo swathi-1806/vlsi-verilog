58d825188da5ed8e6cf8223b235f88f2853535c4
swathi bawge <swathiveda4632@gmail.com>
2026-02-03 13:44:52 +0530

    Update ha.v

--- ha.v
+++ ha.v
@@ -5,6 +5,21 @@
  assign co=a&b;
  endmodule
 
+//testbench code
+module top;
+reg a,b;
+wire s,co;
+half_adder dut(a,b,s,co);
+  initial begin
+    repeat(10)begin
+    {a,b}=$random;
+	#1;
+    $monitor("/t--> a=%b b=%b||sum=%b carry=%b",a,b,s,co);
+    end
+  end
+endmodule
+
+
 /* 
 # /t--> a=0 b=1||sum=1 carry=0
 # /t--> a=0 b=1||sum=1 carry=0
@@ -17,5 +32,6 @@
 # /t--> a=0 b=1||sum=1 carry=0
 # /t--> a=0 b=1||sum=1 carry=0
 */
+
 
 
