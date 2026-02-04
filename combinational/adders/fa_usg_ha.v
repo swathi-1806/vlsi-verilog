fce17ab20ed112af0c9f4bf70bbddf284e11eb87
swathi bawge <swathiveda4632@gmail.com>
2026-02-04 23:14:35 +0530

    Update fa_usg_ha.v

--- combinational/adders/fa_usg_ha.v
+++ combinational/adders/fa_usg_ha.v
@@ -13,3 +13,35 @@ half_adder u2(.a(a),.b(b),.sum(sum),.carry(w3));
 or g1(carry,w2,w3);
 endmodule
 
+//testbench code
+module top;
+reg a,b,cin;
+wire sum,carry;
+
+full_adder dut(a,b,cin,sum,carry);
+initial begin
+repeat(10)begin
+{a,b,cin}=$random;
+#1;
+$display("a=%b b=%b cin=%b||sum=%b carry=%b",a,b,cin,sum,carry);
+end
+end
+endmodule
+
+
+/*
+output
+
+# a=1 b=0 cin=0||sum=1 carry=0
+# a=0 b=0 cin=1||sum=0 carry=0
+# a=0 b=0 cin=1||sum=0 carry=0
+# a=0 b=1 cin=1||sum=1 carry=0
+# a=1 b=0 cin=1||sum=1 carry=0
+# a=1 b=0 cin=1||sum=1 carry=0
+# a=1 b=0 cin=1||sum=1 carry=0
+# a=0 b=1 cin=0||sum=1 carry=0
+# a=0 b=0 cin=1||sum=0 carry=0
+# a=1 b=0 cin=1||sum=1 carry=0
+*/
+
+
