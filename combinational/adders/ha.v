387996897ecaea78889f8b422215b1db39ef6e59
swathi bawge <swathiveda4632@gmail.com>
2026-02-04 23:07:00 +0530

    Update ha.v

--- combinational/adders/ha.v
+++ combinational/adders/ha.v
@@ -1,1 +1,23 @@
+//half adder in dataflow method
 
+module half_adder(a,b,sum,carry);
+input a,b;
+output sum,carry;
+assign sum=a^b;
+assign carry=a&b;
+endmodule
+
+
+//testbench code
+module top;
+     reg a,b;
+     wire sum,carry;
+	 half_adder dut(a,b,sum,carry);
+   initial begin
+      repeat(10)begin
+        {a,b}=$random;
+        #1;
+        $display("\t a=%0b,b=%0b,sum=%0b,carry=%0b",a,b,sum,carry);
+      end
+   end
+endmodule
