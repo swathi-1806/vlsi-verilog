aa03ebb5f19a7d023d592fa5a30bb34e095483b5
swathi bawge <swathiveda4632@gmail.com>
2026-02-28 23:02:35 +0530

    Update inverted_half_pyramid.v

--- star_pattern/inverted_half_pyramid.v
+++ star_pattern/inverted_half_pyramid.v
@@ -1,1 +1,19 @@
+module tb;
+  integer i,j;
+  //integer n=5;
+    initial begin
+      for(i=5;i>=1;i=i-1)begin
+        for(j=1;j<=i;j=j+1)
+          $write("*");
+          $display();
+      end
+    end
+endmodule
 
+/*
+***** 
+ **** 
+  *** 
+   ** 
+    * 
+*/
