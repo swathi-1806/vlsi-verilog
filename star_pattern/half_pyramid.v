106398a531912a0870ec887aa83962c344c4aed5
swathi bawge <swathiveda4632@gmail.com>
2026-02-28 18:28:32 +0530

    Update half_pyramid.v

--- star_pattern/half_pyramid.v
+++ star_pattern/half_pyramid.v
@@ -1,1 +1,35 @@
-module tb
+/*
+Rows → outer loop
+Columns → inner loop
+Condition → when to print "*" and when to print space
+-------------------------------------------
+for(i = 1 to rows)
+{
+    for(j = 1 to columns)
+    {
+        print something
+    }
+    go to next line
+}
+------------------------------------------
+*/
+
+module tb;
+  integer i,j;
+  integer n=5;
+    initial begin
+      for(i=0;i<n;i=i+1)begin
+        for(j=0;j<=i;j=j+1)
+          $write("*");
+          $display();
+      end
+    end
+endmodule
+/*
+output
+ * 
+ ** 
+ *** 
+ **** 
+ ***** 
+ */
