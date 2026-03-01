e97c46d2a1f2bb6663fe719943a59fdd3f8f104d
swathi bawge <swathiveda4632@gmail.com>
2026-03-01 21:07:08 +0530

    Update hallow_triangle.v

--- star_pattern/hallow_triangle.v
+++ star_pattern/hallow_triangle.v
@@ -1,1 +1,22 @@
+module tb;
+  integer i,j;
+  initial begin
+    for(i=1;i<=5;i=i+1)begin
+      for(j=1;j<=i;j=j+1)begin
+        if(i==1||i==5||j==1||i==j)
+          $write("* ")
+          else
+            $write("  ");
+      end
+      $write("\n");
+    end
+  end
+endmodule
 
+/*
+ * 
+ * * 
+ *   * 
+ *     * 
+ * * * * * 
+*/
