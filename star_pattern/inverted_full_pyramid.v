3dba67b2734b6366cfe8c7e1e816a80249df961a
swathi bawge <swathiveda4632@gmail.com>
2026-03-01 20:38:05 +0530

    Update inverted_full_pyramid.v

--- star_pattern/inverted_full_pyramid.v
+++ star_pattern/inverted_full_pyramid.v
@@ -1,1 +1,22 @@
+module tb;
+  integer i,j,k;
+  initial begin
+    for(i=5;i>=1;i=i-1)begin
+      for(j=1;j<=5-i;j=j+1)begin
+        $write(" ");
+      end
+      for(k=1;k<=(2*i-1);k=k+1)begin
+        $write(" *");
+      end
+      $write("\n");
+    end
+  end
+endmodule
 
+/*
+ * * * * * * * * *
+   * * * * * * *
+    * * * * *
+     * * *
+       *
+*/
