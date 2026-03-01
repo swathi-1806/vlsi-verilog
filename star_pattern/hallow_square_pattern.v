c3e3f5b1f4b345ddf0ef337340b1576774605a77
swathi bawge <swathiveda4632@gmail.com>
2026-03-01 20:46:01 +0530

    Update hallow_square_pattern.v

--- star_pattern/hallow_square_pattern.v
+++ star_pattern/hallow_square_pattern.v
@@ -1,1 +1,12 @@
-
+module tb;
+  integer i,j;
+  initial begin
+    for(i=1;i<=5;i=i+1)begin
+      for(j=1;j<=5;j=j+1)begin
+        if(i==1||i==5||j==1||j==5)
+          $write("* ");
+        else
+          $write(" ");
+      end
+    end
+  endmodule
