838e3cc57c3a6162f6d0aeb1e80177e0b4d3b1ce
swathi bawge <swathiveda4632@gmail.com>
2026-03-01 20:50:39 +0530

    Fix output formatting in hollow square pattern

--- star_pattern/hallow_square_pattern.v
+++ star_pattern/hallow_square_pattern.v
@@ -4,9 +4,19 @@ module tb;
     for(i=1;i<=5;i=i+1)begin
       for(j=1;j<=5;j=j+1)begin
         if(i==1||i==5||j==1||j==5)
-          $write("* ");
+          $write("*");
         else
           $write(" ");
       end
     end
   endmodule
+
+    /*
+    
+* * * * *
+*       *
+*       *
+*       *
+* * * * *
+
+    */
