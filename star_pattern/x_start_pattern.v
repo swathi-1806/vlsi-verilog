31afb220b42a33300ba06f2d16dcd212878f9e1c
swathi bawge <swathiveda4632@gmail.com>
2026-03-02 11:51:45 +0530

    Update x_start_pattern.v

--- star_pattern/x_start_pattern.v
+++ star_pattern/x_start_pattern.v
@@ -1,1 +1,27 @@
+module tb;
+  integer i,j;
+  integer n=5;
+    initial begin
+      for(i=1;i<=n;i=i+1)begin
+        for(j=1;j<=n;j=j+1)begin
+          if(i==j||j==(n-i+1))
+          $write("*");
+          else
+            $write("  ");
+          end
+        $write("\n");
+         // $display();
+      end
+    end
+endmodule
 
+/*
+output:-
+
+ *      *
+   *  *  
+     *    
+   *  *  
+ *      *
+
+*/
