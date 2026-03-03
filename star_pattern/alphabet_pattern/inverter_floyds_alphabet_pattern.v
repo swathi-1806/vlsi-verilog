cc5b1ed384a3b22890392f5abaa6514601316b0c
swathi bawge <swathiveda4632@gmail.com>
2026-03-03 11:46:42 +0530

    Implement inverter Floyd's alphabet pattern module
    Added a testbench module to generate inverted Floyd's alphabet pattern.

--- star_pattern/alphabet_pattern/inverter_floyds_alphabet_pattern.v
+++ star_pattern/alphabet_pattern/inverter_floyds_alphabet_pattern.v
@@ -24,3 +24,31 @@ ST
 U
 
 */
+
+/*
+Another model:-
+
+module tb;
+  integer i,j;
+  reg [7:0]ch;
+    initial begin
+      ch="U";
+      for(i=1;i<=6;i=i+1)begin
+        for(j=6;j>=i;j=j-1)begin
+          $write("%s",ch);
+          ch=ch-1;
+        end
+        $write("\n");
+      end
+    end
+endmodule
+--------------------------------------------
+ UTSRQP
+ ONMLK
+ JIHG
+ FED
+ CB
+ A
+--------------------------------------------
+
+*/
