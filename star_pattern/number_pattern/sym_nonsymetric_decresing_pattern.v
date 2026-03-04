a9bd4d6431a01d2e4d4af667745289520806c9ba
swathi bawge <swathiveda4632@gmail.com>
2026-03-04 19:24:05 +0530

    Implement testbench for non-symmetric decreasing pattern
    Added a testbench to generate a non-symmetric decreasing number pattern.

--- star_pattern/number_pattern/sym_nonsymetric_decresing_pattern.v
+++ star_pattern/number_pattern/sym_nonsymetric_decresing_pattern.v
@@ -1,1 +1,31 @@
 
+module tb;
+  integer i,j;
+  int num;
+    initial begin
+      num=10;
+      for(i=1;i<=10;i=i+1)begin
+        for(j=1;j<=i;j=j+1)begin
+          $write("%d",num);
+        end
+        $write("\n");
+        num=num-1;
+      end
+    end
+endmodule
+/*
+OUTPUT:-
+          10
+            9          9
+           8          8          8
+           7          7          7          7
+           6          6          6          6          6
+           5          5          5          5          5          5
+           4          4          4          4          4          4          4
+           3          3          3          3          3          3          3          3
+           2          2          2          2          2          2          2          2          2
+           1          1          1          1          1          1          1          1          1          1
+*/
+/*
+
+*/
