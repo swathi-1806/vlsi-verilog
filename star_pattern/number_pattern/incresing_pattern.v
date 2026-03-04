27a07326130dd81c48e6c35a41fc5d2bee018e7f
swathi bawge <swathiveda4632@gmail.com>
2026-03-04 19:13:48 +0530

    Add increasing number pattern in Verilog module

--- star_pattern/number_pattern/incresing_pattern.v
+++ star_pattern/number_pattern/incresing_pattern.v
@@ -1,1 +1,13 @@
-
+module tb;
+  integer i,j;
+  int num;
+    initial begin
+      num=1;
+      for(i=1;i<=10;i=i+1)begin
+        for(j=1;j<=i;j=j+1)begin
+          $write("%d",num);
+        end
+        $write("\n");
+        num=num+1;
+      end
+endmodule
