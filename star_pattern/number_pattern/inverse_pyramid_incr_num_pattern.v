5cf85ecc9c88a58f5bec11c51ff9cce89c81d1e6
swathi bawge <swathiveda4632@gmail.com>
2026-03-05 16:18:03 +0530

    Create inverse pyramid pattern with incrementing numbers
    Added a testbench module to generate an inverse pyramid pattern with incrementing numbers.

--- star_pattern/number_pattern/inverse_pyramid_incr_num_pattern.v
+++ star_pattern/number_pattern/inverse_pyramid_incr_num_pattern.v
@@ -1,1 +1,28 @@
+module tb;
+  integer i,j,k;
+  int num;
+    initial begin
+      num=1;
+      for(i=5;i>=1;i=i-1)begin
+        for(j=1;j<=5-i;j=j+1)begin
+          $write("  ");
+        end
+        for(k=1;k<=(2*i-1);k=k+1)begin
+          $write("%0d ",num);
+        end
+        $write("\n");
+        num=num+1;
+      end
+    end
+endmodule
 
+/*
+OUTPUT:-
+
+ 1 1 1 1 1 1 1 1 1 
+   2 2 2 2 2 2 2 
+     3 3 3 3 3 
+       4 4 4 
+         5 
+         
+*/
