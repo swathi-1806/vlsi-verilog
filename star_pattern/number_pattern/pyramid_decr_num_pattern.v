2c9d3ae7713bead07b701e3f9c317c9d0f5fbdbe
swathi bawge <swathiveda4632@gmail.com>
2026-03-05 16:24:29 +0530

    Update pyramid_decr_num_pattern.v

--- star_pattern/number_pattern/pyramid_decr_num_pattern.v
+++ star_pattern/number_pattern/pyramid_decr_num_pattern.v
@@ -1,1 +1,26 @@
-
+module tb;
+  integer i,j,k;
+  int num;
+    initial begin
+      num=5;
+      for(i=1;i<=5;i=i+1)begin
+        for(j=1;j<=5-i;j=j+1)begin
+          $write("  ");
+        end
+        for(k=1;k<=(2*i-1);k=k+1)begin
+          $write("%0d ",num);
+        end
+        $write("\n");
+        num=num-1;
+      end
+    end
+endmodule
+/*
+OUTPUT:-
+         5 
+       4 4 4 
+     3 3 3 3 3 
+   2 2 2 2 2 2 2 
+ 1 1 1 1 1 1 1 1 1 
+ 
+*/
