5e9b555d307fd2ae331a3faf43df52116466107d
swathi bawge <swathiveda4632@gmail.com>
2026-03-07 19:45:31 +0530

    Update fout_in_out_d_ff.v

--- genvar/fout_in_out_d_ff.v
+++ genvar/fout_in_out_d_ff.v
@@ -1,1 +1,60 @@
+//single d_ff_rtl code
+module d_ff(
+input clk,
+input d,
+output reg q
+);
+always @(posedge clk)
+q <= d;
+endmodule
 
+
+
+//fout d_ff_connected in parallel
+module four_d_ff(
+input clk,
+input [3:0] d,
+output [3:0]q
+);
+  
+genvar i;
+
+generate
+for(i=0;i<4;i=i+1)
+begin : dff_block
+    d_ff u1(
+        .clk(clk),
+        .d(d[i]),
+        .q(q[i])
+    );
+end
+endgenerate
+
+endmodule
+
+//testbench code
+module tb;
+  reg clk;
+  reg [3:0]d;
+  wire [3:0]q;
+  
+  four_d_ff dut(.clk(clk),.d(d),.q(q));
+  
+  //clk generation
+  initial begin
+			clk = 0;
+			forever #5 clk = ~clk;   // proper clock
+   end
+ 
+  
+    initial begin
+      repeat(5)begin
+        d=$random;
+        #10;
+        $display("clk=%b  d=%b  q=%b ",clk,d,q);
+      end
+      
+      $finish;   // stops simulation
+    end
+endmodule
+      
