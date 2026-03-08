5d7e76b729e4e518bdba5754a1ac17605f01ed98
swathi bawge <swathiveda4632@gmail.com>
2026-03-08 20:26:51 +0530

    Update bitwise_operators.v

--- operators/bitwise_operators.v
+++ operators/bitwise_operators.v
@@ -1,2 +1,15 @@
 module top;
-  
+  reg[3:0]a,b,c;
+    initial begin
+      a=4'b1001;
+      b=4'b1101;
+      c=a&b;
+      $display("AND: a=%b b=%b c=%b",a,b,c);
+
+      c=a|b;
+      $display("OR: a=%b b=%b c=%b",a,b,c);
+    end
+endmodule
+
+      
+      
