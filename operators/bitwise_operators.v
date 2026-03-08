a8e5f88abcc416c88786221188df6f8dff4c2aad
swathi bawge <swathiveda4632@gmail.com>
2026-03-08 20:42:19 +0530

    Update bitwise_operators.v

--- operators/bitwise_operators.v
+++ operators/bitwise_operators.v
@@ -7,9 +7,86 @@ module top;
       $display("AND: a=%b b=%b c=%b",a,b,c);
 
       c=a|b;
-      $display("OR: a=%b b=%b c=%b",a,b,c);
+      $display("OR:    a=%b b=%b c=%b",a,b,c);
+
+      c=~(a&b);
+      $display("NAND:  a=%b b=%b c=%b",a,b,c);
+
+      c=~(a|b);
+      $display("NOR:   a=%b b=%b c=%b",a,b,c);
+
+      c=a^b;
+      $display("EX-OR:  a=%b b=%b c=%b",a,b,c);
+
+      c=~(a^b);
+      $display("EX-NOR: a=%b b=%b c=%b",a,b,c);
+      $display("-------------------------------");
+      
+      a=4'bx001;
+      b=4'b1101;
+      c=a&b;
+      $display("AND:   a=%b b=%b c=%b",a,b,c);
+
+      c=a|b;
+      $display("OR:    a=%b b=%b c=%b",a,b,c);
+
+      c=~(a&b);
+      $display("NAND:  a=%b b=%b c=%b",a,b,c);
+
+      c=~(a|b);
+      $display("NOR:   a=%b b=%b c=%b",a,b,c);
+
+      c=a^b;
+      $display("EX-OR: a=%b b=%b c=%b",a,b,c);
+
+      c=~(a^b);
+      $display("EX-NOR: a=%b b=%b c=%b",a,b,c);
+      
+      $display("-------------------------------");
+      
+      a=4'bz001;
+      b=4'b1101;
+      c=a&b;
+      $display("AND:    a=%b b=%b c=%b",a,b,c);
+
+      c=a|b;
+      $display("OR:     a=%b b=%b c=%b",a,b,c);
+
+      c=~(a&b);
+      $display("NAND:   a=%b b=%b c=%b",a,b,c);
+
+      c=~(a|b);
+      $display("NOR:    a=%b b=%b c=%b",a,b,c);
+
+      c=a^b;
+      $display("EX-OR:  a=%b b=%b c=%b",a,b,c);
+
+      c=~(a^b);
+      $display("EX-NOR: a=%b b=%b c=%b",a,b,c);
+      $display("-------------------------------");
+      
+      a=4'bz001;
+      b=4'bz101;
+      c=a&b;
+      $display("AND:    a=%b b=%b c=%b",a,b,c);
+
+      c=a|b;
+      $display("OR:     a=%b b=%b c=%b",a,b,c);
+
+      c=~(a&b);
+      $display("NAND:   a=%b b=%b c=%b",a,b,c);
+
+      c=~(a|b);
+      $display("NOR:    a=%b b=%b c=%b",a,b,c);
+
+      c=a^b;
+      $display("EX-OR:  a=%b b=%b c=%b",a,b,c);
+
+      c=~(a^b);
+      $display("EX-NOR: a=%b b=%b c=%b",a,b,c);
     end
 endmodule
+
 
       
       
