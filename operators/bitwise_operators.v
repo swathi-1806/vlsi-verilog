51f665daaa5628e6ad5e1fd8493dfce2f7266fe3
swathi bawge <swathiveda4632@gmail.com>
2026-03-08 20:43:25 +0530

    Update bitwise_operators.v

--- operators/bitwise_operators.v
+++ operators/bitwise_operators.v
@@ -86,6 +86,36 @@ module top;
       $display("EX-NOR: a=%b b=%b c=%b",a,b,c);
     end
 endmodule
+/*
+OUTPUT:-
+# AND: a=1001 b=1101 c=1001
+# OR:    a=1001 b=1101 c=1101
+# NAND:  a=1001 b=1101 c=0110
+# NOR:   a=1001 b=1101 c=0010
+# EX-OR:  a=1001 b=1101 c=0100
+# EX-NOR: a=1001 b=1101 c=1011
+# -------------------------------
+# AND:   a=x001 b=1101 c=x001
+# OR:    a=x001 b=1101 c=1101
+# NAND:  a=x001 b=1101 c=x110
+# NOR:   a=x001 b=1101 c=0010
+# EX-OR: a=x001 b=1101 c=x100
+# EX-NOR: a=x001 b=1101 c=x011
+# -------------------------------
+# AND:    a=z001 b=1101 c=x001
+# OR:     a=z001 b=1101 c=1101
+# NAND:   a=z001 b=1101 c=x110
+# NOR:    a=z001 b=1101 c=0010
+# EX-OR:  a=z001 b=1101 c=x100
+# EX-NOR: a=z001 b=1101 c=x011
+# -------------------------------
+# AND:    a=z001 b=z101 c=x001
+# OR:     a=z001 b=z101 c=x101
+# NAND:   a=z001 b=z101 c=x110
+# NOR:    a=z001 b=z101 c=x010
+# EX-OR:  a=z001 b=z101 c=x100
+# EX-NOR: a=z001 b=z101 c=x011
+*/
 
 
       
