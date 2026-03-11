17a16add45d7241adfea845d98f836d6fda3d5f0
swathi bawge <swathiveda4632@gmail.com>
2026-03-11 19:52:58 +0530

    Update reduction_or_unary_operators.v

--- operators/reduction_or_unary_operators.v
+++ operators/reduction_or_unary_operators.v
@@ -42,3 +42,30 @@
       #10  $finish;
    end
  endmodule
+/*
+OUTPUT:-
+# ---Bit Wise AND reduction---
+#  &  4'b1001 = 0
+#  &  4'bx111 = x
+#  &  4'bz111 = x
+# --- Bit Wise NAND reduction---
+#  ~& 4'b1001 = 1
+#  ~& 4'bx001 = 1
+#  ~& 4'bz001 = 1
+# ---Bit Wise OR reduction---
+#  |  4'b1001 = 1
+#  |  4'bx000 = x
+#  |  4'bz000 = x
+# ---Bit Wise NOR reduction---
+#  ~| 4'b1001 = 0
+#  ~| 4'bx001 = 0
+#  ~| 4'bz001 = 0
+# ---Bit Wise XOR reduction---
+#  ^  4'b1001 = 0
+#  ^  4'bx001 = x
+#  ^  4'bz001 = x
+# ---Bit Wise XNOR---
+#  ~^ 4'b1001 = 1
+#  ~^ 4'bx001 = x
+#  ~^ 4'bz001 = x
+*/
