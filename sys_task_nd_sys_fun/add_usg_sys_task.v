e39335dbdaec8c9de04c0ae60217181c0aec4563
swathi bawge <swathiveda4632@gmail.com>
2026-03-13 20:49:10 +0530

    Update add_usg_sys_task.v

--- sys_task_nd_sys_fun/add_usg_sys_task.v
+++ sys_task_nd_sys_fun/add_usg_sys_task.v
@@ -1,1 +1,24 @@
+module tb;
+  integer p,q,r,s;
 
+  task add(input reg[7:0]p,q,output reg[7:0]r,s);
+    begin
+      r=p+q;
+      $display("ADD:  p=%0d  q=%0d  r=%0d",p,q,r);
+      s=p-q;
+      $display("SUB:  p=%0d  q=%0d  s=%0d",p,q,s);
+      $display("--------------------------");
+    end
+  endtask
+
+  initial begin
+    
+      p=50;
+      q=30;
+      add(p,q,r,s);
+      #10;
+      p=100;
+      q=150;
+      add(p,q,r,s);
+  end
+endmodule
