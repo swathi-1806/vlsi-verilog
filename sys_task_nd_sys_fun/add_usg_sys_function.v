62754a34ba420adc2f91d14c76c22b7fcfcb3bdc
swathi bawge <swathiveda4632@gmail.com>
2026-03-13 20:58:51 +0530

    Update add_usg_sys_function.v

--- sys_task_nd_sys_fun/add_usg_sys_function.v
+++ sys_task_nd_sys_fun/add_usg_sys_function.v
@@ -1,1 +1,20 @@
+module tb;
+  reg[7:0]p,q;
+  reg[7:0]r;
+  function reg[7:0]sum(input reg[7:0]a,b);
+    begin
+    sum=a+b;
+    end
+  endfunction
 
+  initial begin
+    repeat(5)begin
+      p=$urandom_range(10,15);
+      q=$urandom_range(50,20);
+      r=sum(p,q);
+      #10;
+      $display("p=%0d  q=%0d  r=%0d",p,q,r);
+      $display("---------------------");
+    end
+  end
+endmodule
