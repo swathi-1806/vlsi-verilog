d384e74fd47c66ecac0bc960401e62144ee0d4ca
swathi bawge <swathiveda4632@gmail.com>
2026-03-24 22:32:14 +0530

    Update example1.v

--- fork_and_join/example1.v
+++ fork_and_join/example1.v
@@ -1,1 +1,24 @@
+module tb;
+  initial begin
+    $display("%0t-->start of entry",$time);
+    #2;
+    $display("%0t-->entry-1",$time);
+      #1;
+    $display("%0t-->entry-2",$time);
+    #3 $display("%0t-->entry-3",$time);
+    $display("%0t-->entry-4",$time);
+  end
+  initial begin
+  $display("%0t-->end of entry",$time);
+  end
+endmodule
 
+/*
+output:
+# 0-->start of entry
+# 0-->end of entry
+# 2-->entry-1
+# 3-->entry-2
+# 6-->entry-3
+# 6-->entry-4
+*/
