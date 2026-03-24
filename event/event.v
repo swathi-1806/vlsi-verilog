dbe8e1d656ab405c873ce1c17a8df9a4a7e99182
swathi bawge <swathiveda4632@gmail.com>
2026-03-24 22:06:34 +0530

    Update event.v

--- event/event.v
+++ event/event.v
@@ -1,1 +1,27 @@
+module tb;
+  event e1,e2;
+  initial begin
+    @e1;
+    $display("entry-1");
+    ->e2;
+  end
+  initial begin
+    @e2;
+    $display("entry-2");
+    $display("all entries are completed");
+  end
+  initial begin
+    $display("entry-3");
+    ->e1;
+  end
+endmodule
 
+/*
+output:
+# run -all
+# entry-3
+# entry-1
+# entry-2
+# all entries are completed
+*/
+    
