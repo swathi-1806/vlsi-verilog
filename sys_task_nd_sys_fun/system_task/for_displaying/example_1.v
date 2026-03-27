18bde9c56ad06628cc9c37b723dd74b515ce649e
swathi bawge <swathiveda4632@gmail.com>
2026-03-27 22:03:57 +0530

    Update example_1.v

--- sys_task_nd_sys_fun/system_task/for_displaying/example_1.v
+++ sys_task_nd_sys_fun/system_task/for_displaying/example_1.v
@@ -1,1 +1,18 @@
-
+module tb;
+  reg[3:0] a,b,c,d;
+  
+    initial begin
+      repeat(5)begin
+        {a,b,c,d}=$random;
+        #1;
+        $display("---------$display------");
+        $display("a=%0b",a);
+        $display("---------$monitor------");
+        $monitor("b=%0b",b);
+        $display("---------$write------");
+        $write("c=%0b",c);
+        $display("---------$strobe------");
+        $strobe("d=%0b",d);
+      end
+    end
+endmodule
