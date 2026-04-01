9aab9d20e3163933c7214b309fd805ccaaf13ff4
swathi bawge <swathiveda4632@gmail.com>
2026-04-01 23:32:06 +0530

    Update d_ff.v

--- sequential/flip_flop/d_ff.v
+++ sequential/flip_flop/d_ff.v
@@ -1,10 +1,10 @@
 module d_ff(clk,rst,d,q);
 input clk,rst,d;
-output q;
+output reg q;
 
 always@(posedge clk)begin
 	if(rst==1)
-		q<=q;
+		q<=1'b0;
 	else
 		q<=d;
 end
@@ -22,16 +22,18 @@ d_ff dut(clk,rst,d,q);
 	end
 	initial begin
 		rst=1;
-		repeat(2)@posedge clk;
+		repeat(2)@(posedge clk)
 		rst=0;
 	end
 	initial begin
-		repeat(10)begin
-			d=$random;
-			#1;
-			$display("clk=%b rst=%b || d=%b  q=%b",clk,rst,d,q);
-		end
-	end
+		repeat(10) begin
+   			 @(negedge clk);
+   			 d = $random;
+
+   			 @(posedge clk);
+   			 $strobe("time=%0t clk=%b rst=%b d=%b q=%b",
+             $time, clk, rst, d, q);
+  end	end
 	initial begin
 		#200;
 		$finish();
@@ -39,5 +41,14 @@ d_ff dut(clk,rst,d,q);
 endmodule
 
 /*
-
+# time=15 clk=1 rst=0 d=0 q=0
+# time=25 clk=1 rst=0 d=1 q=1
+# time=35 clk=1 rst=0 d=1 q=1
+# time=45 clk=1 rst=0 d=1 q=1
+# time=55 clk=1 rst=0 d=1 q=1
+# time=65 clk=1 rst=0 d=1 q=1
+# time=75 clk=1 rst=0 d=1 q=1
+# time=85 clk=1 rst=0 d=0 q=0
+# time=95 clk=1 rst=0 d=1 q=1
+# time=105 clk=1 rst=0 d=1 q=1
 */
