ffd65f9553813ca6c1db5032c7cd2e504e81f1ee
swathi bawge <swathiveda4632@gmail.com>
2026-04-15 11:40:49 +0530

    Update hellow_star_pattern.v

--- rivision/hellow_star_pattern.v
+++ rivision/hellow_star_pattern.v
@@ -13,6 +13,25 @@ module tb;
     end
   end
 endmodule
+/*
+another method:=
+------------------------------------
+module tb;
+  integer i,j;
+  integer n;
+  initial begin
+    $display("*****");
+    for(i=1;i<4;i=i+1)begin
+      for(j=1;j<2;j=j+1)begin
+        $write("*   *");
+      end
+      $display("");
+    end
+      $display("*****");
+  end
+endmodule
+------------------------------------
+*/
 
 /*
 # *****
